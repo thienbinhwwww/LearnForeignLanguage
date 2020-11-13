c3ed0aad9b0600a399cbe17c979f40502103b715
thienbinhwwww <phong@users.noreply.github.com>
2020-11-14 02:40:56 +0700

    test 1

--- GameShow/app/src/main/java/com/example/gameshow/Activity/FuntionsActivity.java
+++ GameShow/app/src/main/java/com/example/gameshow/Activity/FuntionsActivity.java
@@ -2,8 +2,11 @@ package com.example.gameshow.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.gameshow.PrepareGameActivity;
 import com.example.gameshow.R;
 
 public class FuntionsActivity extends AppCompatActivity {
@@ -12,5 +15,15 @@ public class FuntionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_funtions);
+    }
+
+    public void join(View view){
+        Intent intent = new Intent(FuntionsActivity.this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void click(View view){
+        Intent intent = new Intent(FuntionsActivity.this, PrepareGameActivity.class);
+        startActivity(intent);
     }
 }
--- GameShow/app/src/main/java/com/example/gameshow/Activity/GameActivity.java
+++ GameShow/app/src/main/java/com/example/gameshow/Activity/GameActivity.java
@@ -2,7 +2,9 @@ package com.example.gameshow.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.gameshow.R;
 
@@ -12,5 +14,10 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    public void createNewTeam(View view){
+        Intent intent = new Intent(GameActivity.this, CreateNewTeamActivity.class);
+        startActivity(intent);
     }
 }
